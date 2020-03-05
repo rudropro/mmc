5c391641d98d7f5c8dcf9165f3059c629524e4c5
rudropro <58634478+rudropro@users.noreply.github.com>
2020-03-05 22:18:39 +0530

    Update index.js
    new token

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Client, Collection, RichEmbed } = require("discord.js");
 const { token } = require("./botconfig.json");
-const prefix = "NjgzOTEzMTI3MjE3MDcwMTEy.XmEp1A.Y6QvQgFlxIyXTCqmEhzDEC5Drqc";
+
+const prefix = "NjgzOTEzMTI3MjE3MDcwMTEy.XmErlw.OE8GEua6sr15G6ipvqFg7phfD-c";
+
 const bot = new Client();
  
 var version = "2.O";
