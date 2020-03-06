617baf5b6188a25bbc482d12e34f7539dd19dfea
rudropro <58634478+rudropro@users.noreply.github.com>
2020-03-07 00:02:17 +0530

    ,ucommit

--- events/client/ready.js
+++ events/client/ready.js
@@ -8,7 +8,7 @@ module.exports = bot => {
     let statuses = [
         `${bot.guilds.size} servers!`,
         "!help",
-        "Made by rudrogamer"
+        "Made by rudrogamer",
         `over ${bot.users.size} users!`
     ]
 
