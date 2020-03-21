0e86b8c2ebd2323dd9d48d2c3ddb44b9b14467e3
rudropro <58634478+rudropro@users.noreply.github.com>
2020-03-21 18:57:19 +0530

    lol

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ var version = "2.O";
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
 ["console", "command", "event"].forEach(x => require(`./handlers/${x}`)(bot));
 
-const ytdl = require('ytdl-core');
-
-const queue = new Map();
-
 bot.login(token);
 
 
