983d1251eafb149921c1816ce5028e967fdcf26f
rudropro <58634478+rudropro@users.noreply.github.com>
2020-03-21 17:28:42 +0530

    new modules + commit

--- handlers/console.js
+++ handlers/console.js
@@ -1,7 +1,7 @@
 module.exports = (bot) => {
-let prompt = process.openStdin()
-prompt.addListener("data", res => {
-    let x = res.toString().trim().split(/ +/g)
-        bot.channels.get("555039958121971736").send(x.join(" "));
-    });
-}
+    let prompt = process.openStdin()
+    prompt.addListener("data", res => {
+        let x = res.toString().trim().split(/ +/g)
+            bot.channels.get("676480938581950464");
+    }
+)};
